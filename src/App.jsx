0fcc657abca0872908147770a25e07647c9ac867
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2024-01-09 03:34:13 +0530

    Add skills section to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 function App() {
   return (
-    <main className="bg-black min-h-screen w-full text-neutral-100 box-border">
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="font-outfit">MY RESUME</h1>
-      </div>
-    </main>
+    <>
+      <main className="bg-black min-h-screen w-full text-neutral-100 box-border">
+        <div className="flex justify-center items-center h-screen">
+          <h1 className="font-outfit">MY RESUME</h1>
+        </div>
+      </main>
+      <aside>
+        <h2 className="visually-hidden">My skills</h2>
+        <ul className="icon-list">
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>
+            <img src="" alt="" />
+          </li>
+        </ul>
+      </aside>
+    </>
   );
 }
 
