23b21579ef076b8be65c64e061b317a60a68f221
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2024-01-09 19:49:06 +0530

    Update App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import scss from "./skills/html.svg";
 
 function App() {
   return (
-    <div className="main-grid grid grid-cols-2 min-h-screen w-full bg-primary-800 text-neutral-100">
-      <main className="">
+    <div className="main-grid min-h-dvh grid grid-flow-dense grid-cols-[15rem_auto] items-center w-full bg-primary-800 text-neutral-100 font-outfit">
+      <main className="col-start-2">
         <p className="eyebrow">Skills & Experience</p>
         <h1 className="page-title">My Resume</h1>
         <p className="intro">
-          As an individual with exquisite taste in the finest companies, I’ve
+          As an individual with exquisite taste in the finest companies, I have
           helped represent both client and employer to the highest degree.
         </p>
-        <div className="job-experience">
+        <div className="job-experience grid grid-flow-col">
           <div className="job">
             <p className="year">2021</p>
             <div className="job-info">
@@ -22,8 +22,8 @@ function App() {
               <span className="company">Invinex LLC</span>
             </div>
             <p>
-              This job involved the engineering of sales, which I'm not totally
-              sure what that means, but it's important.
+              This job involved the engineering of sales, which I am not totally
+              sure what that means, but it is important.
             </p>
           </div>
 
@@ -63,28 +63,30 @@ function App() {
           </div>
         </div>
       </main>
-      <aside>
-        <h2 className="visually-hidden font-outfit text-2xl font-bold">
-          My skills
-        </h2>
-        <ul className="icon-list">
-          <li>
+
+      <aside className="col-start-1 self-stretch">
+        <h2 className="visually-hidden">My skills</h2>
+        <ul
+          className="icon-list content-center h-full list-none grid gap-[3rem] ml-[4rem] border-l-[1px] border-orange-500"
+          role="list"
+        >
+          <li className="lists-items">
             <p className="visually-hidden">html</p>
             <img src={html} alt="" />
           </li>
-          <li>
+          <li className="lists-items">
             <p className="visually-hidden">css</p>
             <img src={css} alt="" />
           </li>
-          <li>
-            <p className="visually-hidden">js</p>
-            <img src={js} alt="" />
-          </li>
-          <li>
+          <li className="lists-items">
             <p className="visually-hidden">scss</p>
             <img src={scss} alt="" />
           </li>
-          <li>
+          <li className="lists-items">
+            <p className="visually-hidden">scss</p>
+            <img src={js} alt="" />
+          </li>
+          <li className="lists-items">
             <p className="visually-hidden">react</p>
             <img src={react} alt="" />
           </li>
