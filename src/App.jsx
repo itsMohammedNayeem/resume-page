e37088b762bad0e54c2b43e6f31d295deeb12c81
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2024-01-09 18:35:03 +0530

    Refactor layout and styles in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,62 @@ import scss from "./skills/html.svg";
 
 function App() {
   return (
-    <container className="min-h-screen w-full flex">
-      <main className="text-neutral-100">
-        {/* <h1 className="font-outfit">MY RESUME</h1> */}
+    <div className="main-grid grid grid-cols-2 min-h-screen w-full bg-primary-800 text-neutral-100">
+      <main className="">
+        <p className="eyebrow">Skills & Experience</p>
+        <h1 className="page-title">My Resume</h1>
+        <p className="intro">
+          As an individual with exquisite taste in the finest companies, I’ve
+          helped represent both client and employer to the highest degree.
+        </p>
+        <div className="job-experience">
+          <div className="job">
+            <p className="year">2021</p>
+            <div className="job-info">
+              <h2 className="role">Sales Engineer</h2>
+              <span className="company">Invinex LLC</span>
+            </div>
+            <p>
+              This job involved the engineering of sales, which I'm not totally
+              sure what that means, but it's important.
+            </p>
+          </div>
+
+          <div className="job">
+            <p className="year">2019</p>
+            <div className="job-info">
+              <h2 className="role">Sales Team Lead</h2>
+              <span className="company">Ready Call Center ltd.</span>
+            </div>
+            <p>
+              Working as the sales team lead involved a lot of sales calls, as
+              well as supporting the other people on the sales team.
+            </p>
+          </div>
+
+          <div className="job">
+            <p className="year">2017</p>
+            <div className="job-info">
+              <h2 className="role">IT Recruiter / Support Specialist</h2>
+              <span className="company">Belize Virtual Center ltd.</span>
+            </div>
+            <p>
+              This job involved helping recruit people and supporting the
+              support team.
+            </p>
+          </div>
+          <div className="job">
+            <p className="year">2017</p>
+            <div className="job-info">
+              <h2 className="role">IT Recruiter / Support Specialist</h2>
+              <span className="company">Belize Virtual Center ltd.</span>
+            </div>
+            <p>
+              This job involved helping recruit people and supporting the
+              support team.
+            </p>
+          </div>
+        </div>
       </main>
       <aside>
         <h2 className="visually-hidden font-outfit text-2xl font-bold">
@@ -37,7 +90,7 @@ function App() {
           </li>
         </ul>
       </aside>
-    </container>
+    </div>
   );
 }
 
